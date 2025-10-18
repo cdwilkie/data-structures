6578336c3e1deb417205d57482c7ebe27a6d308e
cdwilkie <80400795+cdwilkie@users.noreply.github.com>
2025-10-18 04:13:19 -0400

    Implemented several functions
    getSize(), getCapacity(), indexing[], default constructor

--- src/dynamic_array.cpp
+++ src/dynamic_array.cpp
@@ -1,5 +1,22 @@
 #include "./include/dynamic_array.h"
 
-int32_t DynamicArray::getCapacity() const {
+DynamicArray::DynamicArray()
+   : capacity(0), size(0), data(nullptr) {
+   
+}
+
+DynamicArray::~DynamicArray() {
+   delete[] data;
+}
+
+size_t DynamicArray::getCapacity() const {
    return capacity;
+}
+
+size_t DynamicArray::getSize() const {
+   return size;
+}
+
+int32_t& DynamicArray::operator[](size_t index) {
+   return data[index];
 }
