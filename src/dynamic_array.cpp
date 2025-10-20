d451e36cc9ca4e00efca25be0ffd0cae169760f8
cdwilkie <80400795+cdwilkie@users.noreply.github.com>
2025-10-20 11:04:05 -0400

    Added comments

--- src/dynamic_array.cpp
+++ src/dynamic_array.cpp
@@ -1,22 +1,25 @@
 #include "../include/dynamic_array.h"
 
+// Default Constructor
 DynamicArray::DynamicArray()
    : capacity(0), size(0), data(nullptr) {
-   
-}
+}//end DynamicArray()
 
+// Destructor
 DynamicArray::~DynamicArray() {
    delete[] data;
-}
+}//end ~DynamicArray()
 
+//----------------------
+// Accessors
 size_t DynamicArray::getCapacity() const {
    return capacity;
-}
+}//end getCapacity()
 
 size_t DynamicArray::getSize() const {
    return size;
-}
+}//end getSize()
 
 int32_t& DynamicArray::operator[](size_t index) {
    return data[index];
-}
+}//end []
