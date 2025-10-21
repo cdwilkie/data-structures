39e98cec5fe5f5659b4b4f07087e9c30ad2e73fb
cdwilkie <80400795+cdwilkie@users.noreply.github.com>
2025-10-21 06:24:28 -0400

    Implemented Utility Functions

--- src/dynamic_array.cpp
+++ src/dynamic_array.cpp
@@ -8,7 +8,45 @@ DynamicArray::DynamicArray()
 // Destructor
 DynamicArray::~DynamicArray() {
    delete[] data;
+   data = nullptr;
 }//end ~DynamicArray()
+
+
+
+//---------------------
+// Utility
+void DynamicArray::pushBack(int32_t newValue) {
+   if (size == capacity) {
+      size_t newCapacity = (capacity == 0) ? 1 : 2 * capacity;
+      resizeArray(newCapacity);
+   }
+   data[size] = newValue;
+   ++size;
+}//end pushBack()
+
+void DynamicArray::popBack() {
+   if (size > 0) {
+      --size;
+   }
+   size_t threshold = 0.25 * capacity;
+   if (threshold > size) {
+      resizeArray(threshold);
+   }
+
+}//end popBack()
+
+void DynamicArray::resizeArray(size_t newCapacity) {
+   int32_t* newArray = new int[newCapacity];
+   // For all/any data; copy
+   for (size_t i = 0; i < size; ++i) {
+      newArray[i] = data[i];
+   }
+   delete[] data;
+   data = newArray;
+   capacity = newCapacity;
+}//end resizeArray()
+
+
 
 //----------------------
 // Accessors
@@ -23,3 +61,4 @@ size_t DynamicArray::getSize() const {
 int32_t& DynamicArray::operator[](size_t index) {
    return data[index];
 }//end []
+
