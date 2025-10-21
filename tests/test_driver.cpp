e4f6c91372b01a6d2395df7305bb4ddea865fb07
cdwilkie <80400795+cdwilkie@users.noreply.github.com>
2025-10-21 06:23:46 -0400

    Added Constructor test
    Commented out potential test-driver

--- tests/test_driver.cpp
+++ tests/test_driver.cpp
@@ -1,22 +1,65 @@
 #include "../include/dynamic_array.h"
 
 #include <iostream>
+#include <fstream>
 #include <cassert>
+#include <vector>
 
-void testConstructors() {
-   // Dynamic Array Default Constructor
-   DynamicArray arr;
-   assert(arr.getSize() == 0);
-   std::cout << " Default constructor passed \n";
+/* Displays a green pass or red fail to terminal
+* based on assert test
+*/
+void printScore(bool testCondition, const std::string testName) {
+   if (testCondition) {
+      std::cout << "\033[32m" << " PASS " << testName << "\033[0m\n";
+   }
+   else {
+      std::cout << "\033[31m" << " FAIL " << testName << "\033[0m\n";
+   }
+}//end printScore()
+
+void constructorTest() {
+   DynamicArray array;
+
+   std::string testName = "Default Constructor Size Test";
+   bool currentTest = (array.getSize() == 0);
+   printScore(currentTest, testName);
+
+   testName = "Default Constructor Capacity Test";
+   currentTest = (array.getCapacity() == 0);
+   printScore(currentTest, testName);
 }
 
-int main() {
+int main(int argc, char** argv) {
 
-   std::cout << "Running some tests...\n";
-   testConstructors();
+   /*
+   std::cout << "Loading Test Framework...\n";
+   std::string fileName = 
+         (argc < 2) ? "testData.txt" : argv[1];
+   std::ifstream testFile(fileName);
+   if (!testFile.is_open()) {
+      std::cout << "Error loading framework. Aborted\n" << std::endl;
+      return 1;
+   }
+
+   std::cout << "Building Test Framework...\n";
+   std::vector<std::string> testList;
+   std::string testItem;
+   while(std::getline(testFile, testItem)) {
+      testList.push_back(testItem);
+   }
+
+
+   std::cout << "Running Test Framework...\n";
+    
+   DynamicArray testArray;
+   while (testList.size() > 0) {
+      switch
+   }
+
    std::cout << "All testing has concluded.\n";
+   */
 
 
-
+   constructorTest();
    return 0;
 }
