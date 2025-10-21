b65fbd306cd4a380ffb56196f25003727c40cc40
cdwilkie <80400795+cdwilkie@users.noreply.github.com>
2025-10-21 06:25:05 -0400

    Declared utility functions

--- include/dynamic_array.h
+++ include/dynamic_array.h
@@ -11,12 +11,16 @@ class DynamicArray {
       size_t getCapacity() const;
       size_t getSize() const;
       int32_t& operator[](size_t index);
+      void pushBack(int32_t newValue);
+      void popBack();
 
 
    private:
       size_t capacity;
       size_t size;
       int32_t* data;
+
+      void resizeArray(size_t newCapacity);
    
 
 
