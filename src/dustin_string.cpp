398e5fde2b8bdd2b106c821e68dbc8abc3303d4b
cdwilkie <80400795+cdwilkie@users.noreply.github.com>
2025-10-26 01:09:12 -0400

    Cleaned up code

--- src/dustin_string.cpp
+++ src/dustin_string.cpp
@@ -2,20 +2,20 @@
 
 DustinString::DustinString()
    : length(0), capacity(0), data(nullptr) {
-
 }
 
+/* Constructor expects null terminated string
+*/
 DustinString::DustinString(const char* arr) {
    if (arr) {
-   length = findLength(arr);
-   capacity = length + 1;
-   data = new char[capacity];
-   for (size_t i = 0; i < capacity; ++i) {
-      data[i] = arr[i];
-   }
-
-   }
-}
+      length = findLength(arr);
+      capacity = length + 1;
+      data = new char[capacity];
+      for (size_t i = 0; i < capacity; ++i) {
+         data[i] = arr[i];
+      }//end for copy data
+   }//end if arr
+}//end DustinString()
 
 DustinString::~DustinString() {
    delete[] data;
@@ -40,14 +40,15 @@ void DustinString::resizeArray(size_t newCapacity) {
 
 }
 
+// Assumes all string literals are null terminated '\0'
 size_t DustinString::findLength(const char* arr) {
-   // Assumes all string literals are null terminated '\0'
    size_t length = 0;
    while (arr[length] != '\0') {
       ++length;
-   }
+   }//end while null notfound
    return length;
-}
+}//end findLength()
+
 void DustinString::copyArray(const char* arr) {
    if (arr) {
       size_t arrLength = findLength(arr);
