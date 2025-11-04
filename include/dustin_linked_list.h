03c479c95531628b91d70558d1cc6d2563734b09
cdwilkie <80400795+cdwilkie@users.noreply.github.com>
2025-11-04 00:51:05 -0500

    Added function declarations

--- include/dustin_linked_list.h
+++ include/dustin_linked_list.h
@@ -1,4 +1,24 @@
+/* A DustinLinkedList is custom LinkedList class
+*/
+template <typename T>
+struct Node {
+   Node* head;
+   Node* tail;
+   T data;
+};
+
+template <typename T>
 class DustinLinkedList {
+
+   public:
+      DustinLinkedList();
+      ~DustinLinkedList();
+      void pushBack(T);
+      void insertAt(T, size_t);
+      T popBack();
+      T popAt(size_t);
+
+   private:
 
 
 };
