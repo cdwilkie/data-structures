5085f55959bc32dc904765b89fc0034c12f005a0
cdwilkie <cdwilkie@aggies.ncat.edu>
2025-11-10 06:17:01 -0500

    Refactor tests

---
+++ tests/build/CMakeFiles/tests_exec.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests_exec.dir/Users/dustinwilkie/Documents/GitHub/data-structures/src/dustin_string.cpp.o"
+  "CMakeFiles/tests_exec.dir/Users/dustinwilkie/Documents/GitHub/data-structures/src/dustin_string.cpp.o.d"
+  "CMakeFiles/tests_exec.dir/string_test.cpp.o"
+  "CMakeFiles/tests_exec.dir/string_test.cpp.o.d"
+  "tests_exec"
+  "tests_exec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tests_exec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
