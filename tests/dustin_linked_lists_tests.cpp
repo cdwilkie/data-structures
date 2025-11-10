87692c41f47274e5e58984150f17c1bcd0892d69
cdwilkie <cdwilkie@aggies.ncat.edu>
2025-11-10 07:13:49 -0500

    Updated tests with color

--- tests/dustin_linked_lists_tests.cpp
+++ tests/dustin_linked_lists_tests.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "../include/dustin_linked_list.h"
+#include "color.h"
 
 int main() {
 
@@ -12,6 +13,8 @@ int main() {
    newList.pushFront(700);
    
    newList.printList();
-
+   
+   std::cout << Color::GREEN << "TEST\n" << Color::RESET;
+   std::cout << Color::RED << "TEST" << Color::RESET << std::endl;
    return 0;
 }
