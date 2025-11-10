01b13be53553159891dbb066abde974ed9c33c65
cdwilkie <cdwilkie@aggies.ncat.edu>
2025-11-10 07:13:33 -0500

    Created Color
    Added colors for easy use during testing

---
+++ tests/color.h
@@ -0,0 +1,13 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+namespace Color {
+
+    const char RED[] = "\033[31m";
+    const char GREEN[] = "\033[32m";
+    const char RESET[] = "\033[0m\n";
+
+}
+
+
+#endif
